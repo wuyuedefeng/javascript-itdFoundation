2a954d2919e3a35f7b77ea39e00288fb85e68b3a
wangsen <wangs@tongxinyiliao.com>
2015-09-24 16:30:22 +0800

    优化i_serializeObject方法

--- js/ilib/ijquery.js
+++ js/ilib/ijquery.js
@@ -1,12 +1,33 @@
 (function($) {
+	var $$ = $.fn;
 	//将form表单的数据转换成对象
-    $.fn.i_formDataToObject = function() {
-    		var seriaArray = $(this).serializeArray();
-		var paramsObj = {};
-		for (var index in seriaArray){
-			param = seriaArray[index];
-			paramsObj[param["name"]] = param["value"];  
-		}
-		return paramsObj;
+   	$$.i_serializeObject  = function() {
+    		var obj = {};
+	    var arr = this.serializeArray();
+	    $.each(arr, function() {
+	        if (obj[this.name] !== undefined) {
+	            if (!obj[this.name].push) {
+	                obj[this.name] = [obj[this.name]];
+	            }
+	            obj[this.name].push(this.value || '');
+	        } else {
+	            obj[this.name] = this.value || '';
+	        }
+	    });
+	    return obj;
     };
+    //网络请求
+    $$.i_post = function(url, params, successDo, failDo){
+    		$.ajax({
+          type: "POST",
+          url: url,
+          data: params,
+          success: function(data){
+              
+          },
+          error: function(data){
+              
+          }
+      });
+    }
 })(jQuery);
