a2f6847b29b5fc7091a021717909b71eaed1e187
wangsen <wangs@tongxinyiliao.com>
2015-10-09 10:17:25 +0800

    垂直水平居中的样式

---
+++ js/ilib/i_css.js
@@ -0,0 +1,3 @@
+$(function(){
+	$('.i_center_me_verti, .i_center_me_hori_verti').parent().css('position', 'relative');
+});
--- js/ilib/ibase.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-	$('.i_center_me_vertically, .i_center_me_hori_verti').parent().css('position', 'relative');
-});
